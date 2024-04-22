aaa6b81c65d9e55a7c8007f2b31f0b6394487ef4
ptjtammi <pekka.tj.tammi@student.jyu.fi>
2024-04-22 18:13:23 +0300

    toimiva ohjelma, joka tulostaa index.py tiedostosta projektin tiedot

--- osa2/project-reader/src/project.py
+++ osa2/project-reader/src/project.py
@@ -1,20 +1,30 @@
 
 class Project:
-    def __init__(self, name, description, license, dependencies, dev_dependencies):
+    def __init__(self, name, description, license, authors, dependencies, dev_dependencies):
         self.name = name
         self.description = description
         self.license = license
+        self.authors = authors
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
-
+    #muokattu formatointia
     def _stringify_dependencies(self, dependencies):
-        return ", ".join(dependencies) if len(dependencies) > 0 else "-"
+        if dependencies:
+            return "\n".join(f"- {key}" for key in dependencies.keys())
+        return "-"
+    
+    def _stringify_list(self, items):
+        return "\n".join(f"- {item}" for item in items)
 
     def __str__(self):
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
             f"\nLicence: {self.license}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+            f"\n"
+            f"\nAuthors:\n{self._stringify_list(self.authors)}"
+            f"\n"
+            f"\nDependencies:\n{self._stringify_dependencies(self.dependencies)}"
+            f"\n"
+            f"\nDevelopment dependencies:\n{self._stringify_dependencies(self.dev_dependencies)}"
         )
--- osa2/project-reader/src/project_reader.py
+++ osa2/project-reader/src/project_reader.py
@@ -10,15 +10,15 @@ class ProjectReader:
         # tiedoston merkkijonomuotoinen sisältö
         
         content = request.urlopen(self._url).read().decode("utf-8")
-        #print(content)
+        
         toml_string = content
         parsed = toml.loads(toml_string)
+        #toml tiedostosta halututu tiedot
         nimi = parsed['tool']['poetry']['name']
         desci = parsed['tool']['poetry']['description']
         lisenssi = parsed['tool']['poetry']['license']
+        authors = parsed['tool']['poetry']['authors']
         riippuvuudet = parsed['tool']['poetry']['dependencies']
         dev_riippuvuudet = parsed['tool']['poetry']['group']['dev']['dependencies']
-        #print(dev_riippuvuudet)        
-      #  print(f"{nimi} ja desci ois {desci} ja ja lisuri onpi {lisenssi}")
-        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project(nimi, desci, lisenssi, riippuvuudet, dev_riippuvuudet)
+                # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        return Project(nimi, desci, lisenssi, authors, riippuvuudet, dev_riippuvuudet)
