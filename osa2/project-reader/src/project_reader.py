0c0f611d6d5e8275134f1ec9a21baa1120cc9685
ptjtammi <pekka.tj.tammi@student.jyu.fi>
2024-04-22 17:56:21 +0300

    Toml käsitelty ja viedään näppärästi projektioliolle.

--- osa2/project-reader/src/project_reader.py
+++ osa2/project-reader/src/project_reader.py
@@ -1,6 +1,6 @@
 from urllib import request
 from project import Project
-
+import toml
 
 class ProjectReader:
     def __init__(self, url):
@@ -8,8 +8,17 @@ class ProjectReader:
 
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
+        
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
-
+        #print(content)
+        toml_string = content
+        parsed = toml.loads(toml_string)
+        nimi = parsed['tool']['poetry']['name']
+        desci = parsed['tool']['poetry']['description']
+        lisenssi = parsed['tool']['poetry']['license']
+        riippuvuudet = parsed['tool']['poetry']['dependencies']
+        dev_riippuvuudet = parsed['tool']['poetry']['group']['dev']['dependencies']
+        #print(dev_riippuvuudet)        
+      #  print(f"{nimi} ja desci ois {desci} ja ja lisuri onpi {lisenssi}")
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(nimi, desci, lisenssi, riippuvuudet, dev_riippuvuudet)
